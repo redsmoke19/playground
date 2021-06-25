b8f5a509abcc67a22e1547bf171ef8ffe1c88562
redsmoke19 <redsmoke19@gmail.com>
2021-06-25 12:04:41 +0000

    Deploying to gh-pages from @ redsmoke19/playground@e67169d1d482e852ae3f49ab30aba944b50a804d 🚀

--- gulp/tasks/fonts.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-
-// Копируем все шрифты из папки dev в dist
-
-module.exports = function fonts() {
-  return gulp
-    .src('dev/static/fonts/**/*.*')
-    .pipe(gulp.dest('dist/static/fonts'));
-};
